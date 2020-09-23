4dd3b7bed0c99a6ee52312d922da72a421596a3f
Ricky Chu <qs4869@outlook.com>
2020-09-23 17:27:42 +0800

    update 5 cpp

---
+++ 5.最长回文子串.cpp
@@ -0,0 +1,64 @@
+#include<string>
+#include<vector>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=5 lang=cpp
+ *
+ * [5] 最长回文子串
+ *
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (31.73%)
+ * Likes:    2715
+ * Dislikes: 0
+ * Total Accepted:    383.7K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"babad"'
+ *
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * 
+ * 示例 1：
+ * 
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入: "cbbd"
+ * 输出: "bb"
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        string ans;
+        for(int left = 0;left < n;left++) {
+            for(int i = 0;i + left < n;i++) {
+                int j = i + left;
+                if(left == 0) {
+                    dp[i][j] = 1;
+                }
+                else if(left == 1) {
+                    dp[i][j] = (s[i] == s[j]);
+                }
+                else {
+                    dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
+                }
+                if(dp[i][j] && left + 1> ans.size()) {
+                    ans = s.substr(i, left + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
