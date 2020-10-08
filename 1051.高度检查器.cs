4e9cb4429b03cdb43212fb12c082814c2b382c78
Ricky Chu <qs4869@outlook.com>
2020-10-08 15:27:31 +0800

    update 1051 cs

---
+++ 1051.高度检查器.cs
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=1051 lang=csharp
+ *
+ * [1051] 高度检查器
+ *
+ * https://leetcode-cn.com/problems/height-checker/description/
+ *
+ * algorithms
+ * Easy (73.43%)
+ * Likes:    66
+ * Dislikes: 0
+ * Total Accepted:    24.1K
+ * Total Submissions: 32.7K
+ * Testcase Example:  '[1,1,4,2,1,3]'
+ *
+ * 学校在拍年度纪念照时，一般要求学生按照 非递减 的高度顺序排列。
+ * 
+ * 请你返回能让所有学生以 非递减 高度排列的最小必要移动人数。
+ * 
+ * 注意，当一组学生被选中时，他们之间可以以任何可能的方式重新排序，而未被选中的学生应该保持不动。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：heights = [1,1,4,2,1,3]
+ * 输出：3 
+ * 解释：
+ * 当前数组：[1,1,4,2,1,3]
+ * 目标数组：[1,1,1,2,3,4]
+ * 在下标 2 处（从 0 开始计数）出现 4 vs 1 ，所以我们必须移动这名学生。
+ * 在下标 4 处（从 0 开始计数）出现 1 vs 3 ，所以我们必须移动这名学生。
+ * 在下标 5 处（从 0 开始计数）出现 3 vs 4 ，所以我们必须移动这名学生。
+ * 
+ * 示例 2：
+ * 
+ * 输入：heights = [5,1,2,3,4]
+ * 输出：5
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：heights = [1,2,3,4,5]
+ * 输出：0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= heights.length <= 100
+ * 1 <= heights[i] <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public int HeightChecker(int[] heights) {
+        var arr = new int[101];
+        foreach(var height in heights) {
+            arr[height]++;
+        }
+        int count = 0;
+        for(int i = 1, j = 0;i<arr.Length;i++){
+            while(arr[i]-- > 0) {
+                if (heights[j++] != i) count++;
+            }
+        }
+        return count;
+    }
+}
+// @lc code=end
+
