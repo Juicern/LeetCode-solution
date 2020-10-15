e48a896d39f9391e3c7917ef2bfb51222cc95259
Ricky Chu <qs4869@outlook.com>
2020-10-15 20:12:17 +0800

    update cs 341

---
+++ 341.扁平化嵌套列表迭代器.cs
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=341 lang=csharp
+ *
+ * [341] 扁平化嵌套列表迭代器
+ *
+ * https://leetcode-cn.com/problems/flatten-nested-list-iterator/description/
+ *
+ * algorithms
+ * Medium (64.77%)
+ * Likes:    150
+ * Dislikes: 0
+ * Total Accepted:    11.8K
+ * Total Submissions: 18.2K
+ * Testcase Example:  '[[1,1],2,[1,1]]'
+ *
+ * 给你一个嵌套的整型列表。请你设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
+ * 
+ * 列表中的每一项或者为一个整数，或者是另一个列表。其中列表的元素也可能是整数或是其他列表。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: [[1,1],2,[1,1]]
+ * 输出: [1,1,2,1,1]
+ * 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,[4,[6]]]
+ * 输出: [1,4,6]
+ * 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,4,6]。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool IsInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     int GetInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     IList<NestedInteger> GetList();
+ * }
+ */
+public class NestedIterator {
+
+    public NestedIterator(IList<NestedInteger> nestedList) {
+        
+    }
+
+    public bool HasNext() {
+        
+    }
+
+    public int Next() {
+        
+    }
+}
+
+/**
+ * Your NestedIterator will be called like this:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.HasNext()) v[f()] = i.Next();
+ */
+// @lc code=end
+
