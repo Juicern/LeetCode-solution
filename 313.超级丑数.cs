55c402f91c5173f46dbfe1064c5f4836f64e98ed
RickyChu <qs4869@outlook.com>
2020-11-11 20:55:00 +0800

    update 313 cs

---
+++ 313.超级丑数.cs
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=313 lang=csharp
+ *
+ * [313] 超级丑数
+ *
+ * https://leetcode-cn.com/problems/super-ugly-number/description/
+ *
+ * algorithms
+ * Medium (63.79%)
+ * Likes:    126
+ * Dislikes: 0
+ * Total Accepted:    11.8K
+ * Total Submissions: 18.5K
+ * Testcase Example:  '12\n[2,7,13,19]'
+ *
+ * 编写一段程序来查找第 n 个超级丑数。
+ * 
+ * 超级丑数是指其所有质因数都是长度为 k 的质数列表 primes 中的正整数。
+ * 
+ * 示例:
+ * 
+ * 输入: n = 12, primes = [2,7,13,19]
+ * 输出: 32 
+ * 解释: 给定长度为 4 的质数列表 primes = [2,7,13,19]，前 12
+ * 个超级丑数序列为：[1,2,4,7,8,13,14,16,19,26,28,32] 。
+ * 
+ * 说明:
+ * 
+ * 
+ * 1 是任何给定 primes 的超级丑数。
+ * 给定 primes 中的数字以升序排列。
+ * 0 < k ≤ 100, 0 < n ≤ 10^6, 0 < primes[i] < 1000 。
+ * 第 n 个超级丑数确保在 32 位有符整数范围内。
+ * 
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public int MaxProduct(string[] words) {
+        int max=0;
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int k = i+1; k < words.Length; k++)
+            {      
+                if (i!=k)
+                {   
+                    var m = words[i].Distinct(); 
+                    var n = words[k].Distinct();
+                    var lm = m.Intersect(n).Any();
+                    if(!lm){
+                        max= Math.Max(max,words[i].Length*words[k].Length);
+                    }
+                }
+            }
+        }      
+        return max;
+    }   
+}
+// @lc code=end
+
