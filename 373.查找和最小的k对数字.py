8e1c6b0904d875eb0688d2f4bda452a91676d56d
RickyChu <qs4869@outlook.com>
2020-11-12 13:42:03 +0800

    update 373 py

--- 373.查找和最小的k对数字.py
+++ 373.查找和最小的k对数字.py
@@ -47,6 +47,6 @@
 # @lc code=start
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        return sorted(itertools.product(nums1, nums2), key=sum)[:k]
+        return map(list, heapq.nsmallest(k, itertools.product(nums1, nums2), key=sum))
 # @lc code=end
 
