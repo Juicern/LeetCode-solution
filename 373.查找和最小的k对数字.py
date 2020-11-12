1a1b2a36a56cadaaadffb0ced8f9607b4078edba
RickyChu <qs4869@outlook.com>
2020-11-12 14:12:01 +0800

    update 373 py

--- 373.查找和最小的k对数字.py
+++ 373.查找和最小的k对数字.py
@@ -45,8 +45,23 @@
 #
 
 # @lc code=start
+import heapq
 class Solution:
-    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        return map(list, heapq.nsmallest(k, itertools.product(nums1, nums2), key=sum))
+    def kSmallestPairs(self, nums1, nums2, k):
+        queue = []
+        def push(i, j):
+            if i < len(nums1) and j < len(nums2):
+                heapq.heappush(queue, [nums1[i] + nums2[j], i, j])
+        push(0, 0)
+        pairs = []
+        while queue and len(pairs) < k:
+            _, i, j = heapq.heappop(queue)
+            pairs.append([nums1[i], nums2[j]])
+            push(i, j + 1)
+            if j == 0:
+                push(i + 1, 0)
+        return pairs
+
+
 # @lc code=end
 
