996b23e471c998d281cd46557d2120188e7fe1ed
RickyChu <qs4869@outlook.com>
2020-11-13 21:51:14 +0800

    update 789 js

---
+++ dist/789.逃脱阻碍者.dev.js
@@ -0,0 +1,116 @@
+"use strict";
+
+/*
+ * @lc app=leetcode.cn id=789 lang=javascript
+ *
+ * [789] 逃脱阻碍者
+ *
+ * https://leetcode-cn.com/problems/escape-the-ghosts/description/
+ *
+ * algorithms
+ * Medium (60.66%)
+ * Likes:    22
+ * Dislikes: 0
+ * Total Accepted:    3.1K
+ * Total Submissions: 5.1K
+ * Testcase Example:  '[[1,0],[0,3]]\n[0,1]'
+ *
+ * 你在进行一个简化版的吃豆人游戏。你从 [0, 0] 点开始出发，你的目的地是 target = [xtarget, ytarget]
+ * 。地图上有一些阻碍者，以数组 ghosts 给出，第 i 个阻碍者从 ghosts[i] = [xi, yi] 出发。所有输入均为 整数坐标 。
+ * 
+ * 每一回合，你和阻碍者们可以同时向东，西，南，北四个方向移动，每次可以移动到距离原位置 1 个单位 的新位置。当然，也可以选择 不动 。所有动作 同时
+ * 发生。
+ * 
+ * 如果你可以在任何阻碍者抓住你 之前
+ * 到达目的地（阻碍者可以采取任意行动方式），则被视为逃脱成功。如果你和阻碍者同时到达了一个位置（包括目的地）都不算是逃脱成功。
+ * 
+ * 只有在你有可能成功逃脱时，输出 true ；否则，输出 false 。
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：ghosts = [[1,0],[0,3]], target = [0,1]
+ * 输出：true
+ * 解释：你可以直接一步到达目的地 (0,1) ，在 (1, 0) 或者 (0, 3) 位置的阻碍者都不可能抓住你。 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：ghosts = [[1,0]], target = [2,0]
+ * 输出：false
+ * 解释：你需要走到位于 (2, 0) 的目的地，但是在 (1, 0) 的阻碍者位于你和目的地之间。 
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：ghosts = [[2,0]], target = [1,0]
+ * 输出：false
+ * 解释：阻碍者可以和你同时达到目的地。 
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：ghosts = [[5,0],[-10,-2],[0,-5],[-2,-2],[-7,1]], target = [7,7]
+ * 输出：false
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：ghosts = [[-1,0],[0,1],[-1,0],[0,1],[-1,0]], target = [0,0]
+ * 输出：true
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * ghosts[i].length == 2
+ * -10^4 i, yi 
+ * 同一位置可能有 多个阻碍者 。
+ * target.length == 2
+ * -10^4 target, ytarget 
+ * 
+ * 
+ */
+// @lc code=start
+
+/**
+ * @param {number[][]} ghosts
+ * @param {number[]} target
+ * @return {boolean}
+ */
+var escapeGhosts = function escapeGhosts(ghosts, target) {
+  var steps = Math.abs(target[0]) + Math.abs(target[1]);
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = ghosts[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var ghost = _step.value;
+      if (Math.abs(ghost[0] - target[0]) + Math.abs(ghost[1] - target[1]) <= steps) return false;
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+        _iterator["return"]();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return true;
+}; // @lc code=end
