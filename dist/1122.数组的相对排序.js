d22a5fdcb49c7459dcf5fac5e765ada345dde732
RickyChu <qs4869@outlook.com>
2020-11-14 15:18:09 +0800

    update 1122 js

---
+++ dist/1122.数组的相对排序.js
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=1122 lang=typescript
+ *
+ * [1122] 数组的相对排序
+ *
+ * https://leetcode-cn.com/problems/relative-sort-array/description/
+ *
+ * algorithms
+ * Easy (66.99%)
+ * Likes:    122
+ * Dislikes: 0
+ * Total Accepted:    38.1K
+ * Total Submissions: 54.4K
+ * Testcase Example:  '[2,3,1,3,2,4,6,7,9,2,19]\n[2,1,4,3,9,6]'
+ *
+ * 给你两个数组，arr1 和 arr2，
+ *
+ *
+ * arr2 中的元素各不相同
+ * arr2 中的每个元素都出现在 arr1 中
+ *
+ *
+ * 对 arr1 中的元素进行排序，使 arr1 中项的相对顺序和 arr2 中的相对顺序相同。未在 arr2 中出现过的元素需要按照升序放在 arr1
+ * 的末尾。
+ *
+ *
+ *
+ * 示例：
+ *
+ * 输入：arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+ * 输出：[2,2,2,1,4,3,3,9,6,7,19]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * arr1.length, arr2.length <= 1000
+ * 0 <= arr1[i], arr2[i] <= 1000
+ * arr2 中的元素 arr2[i] 各不相同
+ * arr2 中的每个元素 arr2[i] 都出现在 arr1 中
+ *
+ *
+ */
+// @lc code=start
+function relativeSortArray(arr1, arr2) {
+    var order = {};
+    for (var i = 0; i < arr2.length; i++) {
+        order[arr2[i]] = i;
+    }
+    arr1.sort(function (a, b) { return (a in order ? order[a] : a + 1001) - (b in order ? order[b] : b + 1001); });
+    return arr1;
+}
+;
+// @lc code=end
