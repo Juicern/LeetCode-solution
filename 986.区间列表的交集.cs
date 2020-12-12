f7728e2e2f704fc8a653054c07bcbf57089abe46
RickyChu <qs4869@outlook.com>
2020-12-12 15:00:34 +0800

    update 986 cs

---
+++ 986.区间列表的交集.cs
@@ -0,0 +1,67 @@
+using System;
+/*
+ * @lc app=leetcode.cn id=986 lang=csharp
+ *
+ * [986] 区间列表的交集
+ *
+ * https://leetcode-cn.com/problems/interval-list-intersections/description/
+ *
+ * algorithms
+ * Medium (65.63%)
+ * Likes:    113
+ * Dislikes: 0
+ * Total Accepted:    11.8K
+ * Total Submissions: 17.9K
+ * Testcase Example:  '[[0,2],[5,10],[13,23],[24,25]]\n[[1,5],[8,12],[15,24],[25,26]]'
+ *
+ * 给定两个由一些 闭区间 组成的列表，每个区间列表都是成对不相交的，并且已经排序。
+ * 
+ * 返回这两个区间列表的交集。
+ * 
+ * （形式上，闭区间 [a, b]（其中 a <= b）表示实数 x 的集合，而 a <= x <=
+ * b。两个闭区间的交集是一组实数，要么为空集，要么为闭区间。例如，[1, 3] 和 [2, 4] 的交集为 [2, 3]。）
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 
+ * 输入：A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+ * 输出：[[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 <= A.length < 1000
+ * 0 <= B.length < 1000
+ * 0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public int[][] IntervalIntersection(int[][] A, int[][] B) {
+        var ans = new List<int[]>();
+        int i = 0;
+        int j =0;
+        while (i < A.Length && j < B.Length) {
+            int low = Math.Max(A[i][0], B[j][0]);
+            int high = Math.Min(A[i][1], B[j][1]);
+            if(low <= high){
+                ans.Add(new int[]{low, high});
+            }
+            if(A[i][1] < B[j][1]) {
+                i++;
+            }
+            else j++;
+        }
+        return ans.ToArray();
+    }
+}
+// @lc code=end
+
