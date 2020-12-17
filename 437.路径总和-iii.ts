db07e67fb3b52ce54a944ee114ad5771172231e3
RickyChu <qs4869@outlook.com>
2020-12-17 20:47:33 +0800

    update 437 ts

---
+++ 437.路径总和-iii.ts
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=437 lang=typescript
+ *
+ * [437] 路径总和 III
+ *
+ * https://leetcode-cn.com/problems/path-sum-iii/description/
+ *
+ * algorithms
+ * Medium (56.43%)
+ * Likes:    683
+ * Dislikes: 0
+ * Total Accepted:    60.1K
+ * Total Submissions: 106.5K
+ * Testcase Example:  '[10,5,-3,3,2,null,11,3,-2,null,1]\n8'
+ *
+ * 给定一个二叉树，它的每个结点都存放着一个整数值。
+ * 
+ * 找出路径和等于给定数值的路径总数。
+ * 
+ * 路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+ * 
+ * 二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+ * 
+ * 示例：
+ * 
+ * root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+ * 
+ * ⁠     10
+ * ⁠    /  \
+ * ⁠   5   -3
+ * ⁠  / \    \
+ * ⁠ 3   2   11
+ * ⁠/ \   \
+ * 3  -2   1
+ * 
+ * 返回 3。和等于 8 的路径有:
+ * 
+ * 1.  5 -> 3
+ * 2.  5 -> 2 -> 1
+ * 3.  -3 -> 11
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function pathSum(root: TreeNode | null, sum: number): number {
+    var ans : number = 0
+    var helper = function(node: TreeNode | null, pre : number, isFirst : boolean) : void {
+        if(node === null) return
+        if(node.val === pre) ans++
+        helper(node.left, pre - node.val, false)
+        helper(node.right, pre - node.val, false)
+        if(isFirst) {
+            helper(node.left, sum, true)
+            helper(node.right, sum, true)
+        }
+    }
+    helper(root, sum, true)
+    return ans
+};
+// @lc code=end
+
