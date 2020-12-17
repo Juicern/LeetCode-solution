66145ca673a4b68d96b3fb55f2042c7e59a76b73
RickyChu <qs4869@outlook.com>
2020-12-17 21:16:33 +0800

    update 437 py

--- 437.路径总和-iii.py
+++ 437.路径总和-iii.py
@@ -14,17 +14,17 @@
 # Testcase Example:  '[10,5,-3,3,2,null,11,3,-2,null,1]\n8'
 #
 # 给定一个二叉树，它的每个结点都存放着一个整数值。
-# 
+#
 # 找出路径和等于给定数值的路径总数。
-# 
+#
 # 路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
-# 
+#
 # 二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
-# 
+#
 # 示例：
-# 
+#
 # root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
-# 
+#
 # ⁠     10
 # ⁠    /  \
 # ⁠   5   -3
@@ -32,14 +32,14 @@
 # ⁠ 3   2   11
 # ⁠/ \   \
 # 3  -2   1
-# 
+#
 # 返回 3。和等于 8 的路径有:
-# 
+#
 # 1.  5 -> 3
 # 2.  5 -> 2 -> 1
 # 3.  -3 -> 11
-# 
-# 
+#
+#
 #
 
 # @lc code=start
@@ -49,23 +49,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
     def pathSum(self, root: TreeNode, sum: int) -> int:
-        def helper(node: TreeNode, pre : int, depth: int) :
-            if not node: return
-            if node.val == pre: self.ans += 1
-            # choose this node
-            helper(node.left, pre - node.val, 0)
-            helper(node.right, pre - node.val, 0)
-            # not choose this node
-            # Represents the depth of the first node that is not taken
-            # In fact, it is to judge whether the root node is not taken
-            if depth:
-                helper(node.left, sum, 1)
-                helper(node.right, sum, 1)
-        self.ans = 0
-        helper(root, sum, 1)
-        return self.ans
-            
-# @lc code=end
+        dict = {0: 1}
+        def helper(node: TreeNode, preSum: int) -> int:
+            if not node:
+                return 0
+            preSum += node.val
+            ans = dict.get(preSum - sum, 0)
+            dict[preSum] = dict.get(preSum, 0) + 1
+            ans += helper(node.left, preSum) + helper(node.right, preSum)
+            dict[preSum] -= 1
+            return ans
+        return helper(root, 0)
 
+
+# @lc code=end
