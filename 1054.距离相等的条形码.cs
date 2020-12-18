1465178907653dbe2609fbba28c0f4fa7a4c7450
RickyChu <qs4869@outlook.com>
2020-12-18 21:35:49 +0800

    update 1054 cs

--- 1054.距离相等的条形码.cs
+++ 1054.距离相等的条形码.cs
@@ -42,24 +42,31 @@
  * 
  * 
  */
-
 // @lc code=start
-public class Solution {
-    public int[] RearrangeBarcodes(int[] barcodes) {
-        Dictionary<int, int> count = new Dictionary<int, int>(); 
-        foreach(var barcode in barcodes) {
-            if(!count.ContainsKey(barcode)) count.Add(barcode, 1);
-            else count[barcode]++;
+public class Solution
+{
+    public int[] RearrangeBarcodes(int[] barcodes)
+    {
+        Dictionary<int, int> count = new Dictionary<int, int>();
+        foreach (var barcode in barcodes)
+        {
+            if (!count.ContainsKey(barcode))
+                count.Add(barcode, 1);
+            else
+                count[barcode]++;
         }
-        count = count.OrderBy(x => -x.Value).ToDictionary(x => x.Key, x => x.Value);
+        count =
+            count.OrderBy(x => -x.Value).ToDictionary(x => x.Key, x => x.Value);
         int index = 0;
         var ans = new int[barcodes.Length];
-        foreach(var key in count.Keys) {
+        foreach (var key in count.Keys)
+        {
             int times = count[key];
-            while(times > 0) {
+            while (times > 0)
+            {
                 ans[index] = key;
                 index += 2;
-                if(index >= barcodes.Length) index = 1;
+                if (index >= barcodes.Length) index = 1;
                 times--;
             }
         }
@@ -67,4 +74,3 @@ public class Solution {
     }
 }
 // @lc code=end
-
