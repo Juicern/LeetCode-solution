4662f2d4eede55f849bf700da2cbcbec7bc96318
RickyChu <qs4869@outlook.com>
2021-01-31 12:25:55 +0800

    update 778

--- 778.水位上升的泳池中游泳.cs
+++ 778.水位上升的泳池中游泳.cs
@@ -65,38 +65,52 @@ using System.Collections.Generic;
 
 // @lc code=start
 public class Solution {
-    int[] dir_x = new int[]{-1, 0, 0, 1};
-    int[] dir_y = new int[]{0, 1, -1, 0};
+    readonly (int, int)[] dirs = new (int, int)[]{(0, -1), (1, 0), (0, 1), (-1, 0)};
     public int SwimInWater(int[][] grid) {
-        int n = grid.Length;
-        int left = grid[0][0];
-        int right = n * n;
-        while(left < right) {
-            int mid = left + (right - left) / 2;
-            var queue = new Queue<int[]>();
-            var seen = new bool[n, n];
-            seen[0, 0] = true;
-            queue.Enqueue(new int[]{0, 0});
-            while(queue.Any()) {
-                var node = queue.Dequeue();
-                if(node[0] == n - 1 && node[1] == n - 1) break;
-                for(int i = 0;i<4;i++) {
-                    int x = node[0] + dir_x[i]; 
-                    int y = node[1] + dir_y[i]; 
-                    if(x >= 0 && x < n  && y >= 0 && y< n && !seen[x, y] && grid[x][y] <= mid) {
-                        queue.Enqueue(new int[]{x, y});
-                        seen[x, y] = true;
-                    }
-                }
-            }
-            if(seen[n - 1, n - 1]) {
-                right = mid;
-            }
-            else {
-                left = mid + 1;
+        var n = grid.Length;
+        var uf = new UnionFind(n * n);
+        var height_pos = new (int, int)[n * n];
+        for(int i = 0; i< n;++i) {
+            for(int j = 0;j<n;++j) {
+                height_pos[grid[i][j]] = (i, j); 
             }
         }
-        return left;
+        for (var height = 0; height  < n * n; ++hFeight) {
+            var (pos_x, pos_y) = height_pos[height];
+            foreach(var (dir_x, dir_y) in dirs) {
+                var new_pos_x = pos_x + dir_x;
+                var new_pos_y = pos_y + dir_y;
+                if(new_pos_x >= 0 && new_pos_x < n && new_pos_y >= 0 && new_pos_y < n && grid[new_pos_x][new_pos_y] <= height) {
+                    uf.Merge(pos_x * n + pos_y, new_pos_x * n + new_pos_y);
+                }
+            }
+            if(uf.IsConnected(0, n * n - 1)) return height;
+        }
+        return -1;
+    }
+}
+public class UnionFind{
+    int[] parent;
+    int n;
+    public UnionFind(int _n) {
+        n = _n;
+        parent = new int[n];
+        for(int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+    public void Merge(int _x, int _y) {
+        var root_x = Find(_x);
+        var root_y = Find(_y);
+        if(root_x == root_y) return;
+        parent[root_x] = root_y;
+    }
+    private int Find(int _x) {
+        if(parent[_x] == _x) return _x;
+        return parent[_x] = Find(parent[_x]);
+    }
+    public bool IsConnected(int _x, int _y) {
+        return Find(_x) == Find(_y);
     }
 }
 // @lc code=end
